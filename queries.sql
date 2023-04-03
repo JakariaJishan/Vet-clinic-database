b537023a662342d70650a49b2ee06fae11652bc7
JakariaJishan <jakariajishan987@gmail.com>
2023-04-04 01:10:55 +0600

    Create table and insert and query data

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+select name from animals where neutered is true and escape_attempts < 3
+select date_of_birth  from animals where name in ('Agumon', 'Pikachu') 
+select name, escape_attempts from animals where weight_kg>10.5
+select * from animals where neutered is true
+select * from animals where name != 'Gabumon'
+select * from animals where weight_kg between 10.4  and 17.3
--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+create table animals(
+	id int primary key generated always as identity,
+	name char(50),
+	date_of_birth date,
+	escape_attempts int,
+	neutered boolean,
+	weight_kg decimal
+)
+
