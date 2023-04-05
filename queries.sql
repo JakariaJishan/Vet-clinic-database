f769f06274227c9a1bd0a58c41492e7671640c7c
JakariaJishan <jakariajishan987@gmail.com>
2023-04-05 21:47:05 +0600

    Make all the changes according to code reviewer

--- queries.sql
+++ queries.sql
@@ -53,9 +53,9 @@ commit ;
 
 
 -- aggregations
-select count(*) from animals
+select count(*) from animals;
 select count (escape_attempts) from animals where escape_attempts = 0;
-select avg(weight_kg) from animals
-select neutered, sum(escape_attempts) from animals group by neutered
-select species, min(weight_kg), max(weight_kg) from animals group by species
+select avg(weight_kg) from animals;
+select neutered, sum(escape_attempts) from animals group by neutered;
+select species, min(weight_kg), max(weight_kg) from animals group by species;
 select species, avg(escape_attempts) from animals where date_of_birth between '1990-01-01' AND '2000-12-31' group by species;
